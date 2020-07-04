a0bb887dba77bdd82444b2e63fa58a0fc3a67fdc
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2020-07-04 19:04:10 +0100

    API warning

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const path = require("path");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
-const users = require("./routes/api/users");
-const riddles = require("./routes/api/riddles");
-const progress = require("./routes/api/progress");
+const users = require('./routes/api/users');
+const riddles = require('./routes/api/riddles');
+const progress = require('./routes/api/progress');
 
 const app = express();
 
@@ -15,32 +15,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB connected"))
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
 
 // Passport config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 // Use Routes
-app.use("/api/users", users);
-app.use("/api/riddles", riddles);
-app.use("/api/progress", progress);
+app.use('/api/users', users);
+app.use('/api/riddles', riddles);
+app.use('/api/progress', progress);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
