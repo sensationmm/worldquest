747eaa1f641c9c03e6bbf2d5b89b20722e3a3072
Kevin Reynolds <kevin@sensarionmultimedia.co.uk>
2023-11-29 11:14:48 -0500

    Register link to Login

--- client/src/containers/Register.tsx
+++ client/src/containers/Register.tsx
@@ -1,8 +1,8 @@
 import * as SecureStore from 'expo-secure-store';
-import React, { Dispatch, SetStateAction, useState } from 'react';
-import { View } from 'react-native';
+import React, { Dispatch, SetStateAction, useContext, useState } from 'react';
+import { Text, View } from 'react-native';
 
-import { ScreenProps } from '../App';
+import { ScreenProps, ThemeContext } from '../App';
 import PageHeader from '../components/page-header';
 import FormInput from '../components/form-input';
 import { ErrorBox } from '../components/box';
@@ -10,12 +10,16 @@ import Button from '../components/button';
 import accountService from '../services/AccountService';
 import progressService from '../services/ProgressService';
 
+import Fonts from '../constants/Fonts';
+import { Link } from '@react-navigation/native';
+
 const Register: React.FC<ScreenProps> = ({ setIsLoading, setIsLoggedIn }) => {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [password2, setPassword2] = useState('');
   const [email, setEmail] = useState('');
   const [error, setError] = useState(undefined);
+  const theme = useContext(ThemeContext);
 
   const AccountService = new accountService();
   const ProgressService = new progressService();
@@ -71,6 +75,13 @@ const Register: React.FC<ScreenProps> = ({ setIsLoading, setIsLoggedIn }) => {
       {error && <ErrorBox>{error}</ErrorBox>}
 
       <Button onClick={onRegister} label={'Register'} disabled={submitDisabled} />
+
+      <Text style={{ ...Fonts(theme).body, ...Fonts(theme).bold, color: 'white', textAlign: 'center' }}>
+        Already have an account?&nbsp;
+        <Link to={{ screen: 'Log In' }} style={{ textDecorationLine: 'underline' }}>
+          Login here
+        </Link>
+      </Text>
     </View>
   );
 };
