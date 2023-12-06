a2f087af376f3fafb0a3b37031bd8d21b697448e
Kevin Reynolds <kevin@sensarionmultimedia.co.uk>
2023-12-06 11:37:23 -0800

    Avatar asset creditation

--- client/src/containers/Info.tsx
+++ client/src/containers/Info.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Linking, Text, TouchableOpacity, View } from 'react-native';
 
 import { ScreenProps } from '../App';
 import Box from '../components/box';
 import PageHeader from '../components/page-header';
+import { Link } from '@react-navigation/native';
 
 const Info: React.FC<ScreenProps> = () => {
   return (
@@ -16,6 +17,20 @@ const Info: React.FC<ScreenProps> = () => {
 
       <Box title={'In-App Purchases'}>
         <Text>Joining the Quest, Buying Clues, etc.</Text>
+      </Box>
+
+      <Box title={'Credits'}>
+        <TouchableOpacity
+          onPress={() =>
+            Linking.openURL(
+              'https://www.freepik.com/free-vector/animal-avatars-flat-design_772910.htm#query=avatar&position=29&from_view=search&track=sph&uuid=f681fb4f-88ae-4ae1-bc5e-a368e648b3e8'
+            ).catch((err) => console.error('An error occurred', err))
+          }
+        >
+          <Text>
+            Avatar images by <Text style={{ textDecorationLine: 'underline' }}>Freepik</Text>.
+          </Text>
+        </TouchableOpacity>
       </Box>
     </View>
   );
